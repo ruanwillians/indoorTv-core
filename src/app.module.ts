f9ed3b8dd0aa399624b048bdf8e9ed9f164aee44
Ruan Willians Mira Nogueira <108192932+ruanwillians@users.noreply.github.com>
2024-09-18 22:44:19 -0300

    Feat: add env local and prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,25 +10,26 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       load: [() => ({
         DATABASE_URL: process.env.DATABASE_URL,
         JWT_SECRET: process.env.JWT_SECRET,
+        NODE_ENV: process.env.NODE_ENV
       })],
       isGlobal: true,
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        url: configService.get<string>('DATABASE_URL'),
-        entities: [],
-        synchronize: true,
-        ssl: {
-          rejectUnauthorized: false 
-        },
-        tls: {
-          rejectUnauthorized: false 
-        },
-        options: { encrypt: false, trustServerCertificate: true },
-      }),
+      useFactory: async (configService: ConfigService) => {
+        const prod = configService.get<string>('NODE_ENV') === 'production';
+
+        return {
+          type: 'postgres',
+          url: configService.get<string>('DATABASE_URL'),
+          entities: [], 
+          synchronize: true, 
+          ssl: prod ? { rejectUnauthorized: false } : false, 
+          tls: prod ? { rejectUnauthorized: false } : false, 
+          options: { encrypt: false, trustServerCertificate: true },
+        };
+      },
     }),
   ],
   controllers: [AppController],
